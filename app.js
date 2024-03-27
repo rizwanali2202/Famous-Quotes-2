504170dac603ffb8c4185437d66488566a54320f
Rizwan Ali <164035652+rizwanali2202@users.noreply.github.com>
2024-03-27 16:24:10 +0500

    Famous Quotes 2
    you can make new Quotes program via using  typescript

---
+++ app.js
@@ -0,0 +1,5 @@
+"use strict";
+let famous_person = "Quaid azam";
+let Quotes = "work work and always work";
+let message = `${famous_person} once said, ${Quotes}`;
+console.log(message);
---
+++ app.ts
@@ -0,0 +1,5 @@
+let famous_person : string = "Quaid azam";
+let Quotes : string = "work work and always work";
+let message = `${famous_person} once said, ${Quotes}`;
+console.log(message);
+
